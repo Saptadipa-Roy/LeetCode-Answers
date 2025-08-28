e3b5ee2f713016153ac52cece1a91559089fb932
Saptadipa-Roy <132432978+Saptadipa-Roy@users.noreply.github.com>
2025-08-28 11:30:20 +0530

    Create 3446. Sort Matrix by Diagonals.cpp

---
+++ 3446. Sort Matrix by Diagonals.cpp
@@ -0,0 +1,33 @@
+//(Using Map - (i-j) as the key)
+//T.C : O(n^2 * logn)
+//S.C : O(n^2) map stores all elements
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        unordered_map<int,vector<int>> mp;
+        //(i-j) -> diagonal elements
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                int diag = i-j;
+                mp[diag].push_back(grid[i][j]);
+            }
+        }
+        for(auto &it : mp){
+            if(it.first >= 0){//Key
+                sort(begin(it.second),end(it.second));
+            }
+            else{
+                sort(rbegin(it.second),rend(it.second));
+            }
+        }
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                int diag = i-j;
+                grid[i][j] = mp[diag].back();
+                mp[diag].pop_back();
+            }
+        }
+        return grid;
+    }
+};
