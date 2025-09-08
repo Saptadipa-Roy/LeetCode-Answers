e6726d63b7a73bd54fe54a2ec2b1051362cc2ad8
Saptadipa-Roy <132432978+Saptadipa-Roy@users.noreply.github.com>
2025-09-08 18:03:06 +0530

    Create 104. Maximum Depth of Binary Tree.cpp

---
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        //Base Case
+        if(root == NULL){
+            return 0;
+        }
+        int leftHeight = maxDepth(root->left);
+        int rightHeight = maxDepth(root->right);
+        int height = max(leftHeight,rightHeight) + 1;
+        return height;
+    }
+};
