02bbc983c8196d6be76078fd2ce31bd7a11582ea
Saptadipa-Roy <132432978+Saptadipa-Roy@users.noreply.github.com>
2025-09-25 22:43:34 +0530

    Create 120. Triangle.cpp

---
+++ 120. Triangle.cpp
@@ -0,0 +1,18 @@
+//Approach-3 : Bottom Up optimized - O(n) space
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> t = triangle.back();  // last row
+
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
+                t[col] = triangle[row][col] + min(t[col], t[col + 1]);
+            }
+        }
+
+        return t[0];
+    }
+};
